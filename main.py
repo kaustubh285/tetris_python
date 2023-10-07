1715db6c8c8c3dc4e70b4584528fe213c4ecad5e
k_dozee <kaustubh@dozee.io>
2023-10-07 21:49:47 +0100

    for_loop to find current_ones and check them

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+from display import display
 
 
 class Tetris:
@@ -19,23 +20,46 @@ class Tetris:
     def print_grid(self):
         print(self.grid)
 
-    def check_topmost_occupant(self, position):
+    def check_topmost_occupant(self, shape, position):
+        current_row = 0
+        num_rows = len(shape)
+        num_cols = len(shape[0])
+        print(num_rows, num_cols)
+
+        current_x = position
+        current_y = 0
+
+        current_ones = []
+        for i in range(num_rows):
+            for j in range(num_cols):
+                if shape[i][j] == 1:
+                    current_ones.append((i, j))
+        temp_curr = current_ones
+        print(current_ones)
+
+        for i in range(len(current_ones)):
+            current_ones[i] = (current_ones[i][0], current_ones[i][1] + 1)
+
         for idx, row in enumerate(self.grid):
             if sum(row):
-                if row[position - 1] == 1:
-                    print("blockage at ", idx, position - 1)
-                    break
                 if row[position] == 1:
                     print("blockage at ", idx, position)
                     break
                 if row[position + 1] == 1:
                     print("blockage at ", idx, position + 1)
                     break
+            # block did not have any blockage, it has moved to the below row
+
+        return current_row
+
+    def find_down(shape):
         pass
 
     def new_input(self, value: str, position: int):
         print(value, position)
-        self.check_topmost_occupant(position)
+        self.check_topmost_occupant(self.values[value], position)
+        display(self.values[value])
+        display(self.grid)
         pass
 
 
