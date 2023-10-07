3ab75c31aed7de2834757c9074eee9daf599abb6
k_dozee <kaustubh@dozee.io>
2023-10-07 23:18:30 +0100

    X, Y confusion solved, 1 block can be added

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from display import display
 class Tetris:
     def __init__(self) -> None:
         # self.grid = [[0] * 10] * 10
-        self.grid = [[0] * 7] * 3 + [[0] * 5 + [1] * 2] * 3 + [[0] * 3 + [1] * 4] * 3
+        self.grid = [[0] * 7] * 3 + [[0] * 6 + [1] * 1] * 3 + [[0] * 3 + [1] * 4] * 3
         self.values = {
             "q": [[1, 1], [1, 1]],
             "z": [[1, 1, 0], [0, 1, 1]],
@@ -20,46 +20,63 @@ class Tetris:
     def print_grid(self):
         print(self.grid)
 
-    def check_topmost_occupant(self, shape, position):
-        current_row = 0
+    def provide_current_ones(self, shape, position):
         num_rows = len(shape)
         num_cols = len(shape[0])
-        print(num_rows, num_cols)
-
-        current_x = position
-        current_y = 0
-
         current_ones = []
         for i in range(num_rows):
             for j in range(num_cols):
                 if shape[i][j] == 1:
-                    current_ones.append((i, j))
+                    current_ones.append((i, position + j))
+        return current_ones
+
+    def check_position_clash(self, x, y):
+        if len(self.grid) == x + 1:
+            return True
+        try:
+            if self.grid[x + 1][y] == 1:
+                return True
+        except:
+            print("____________________________________")
+            print(x, y)
+            print(len(self.grid), len(self.grid[0]))
+            if self.grid[x - 1][y] == 1:
+                return True
+
+    def check_topmost_occupant(self, shape, position):
+        current_ones = self.provide_current_ones(shape, position)
+
         temp_curr = current_ones
         print(current_ones)
-
-        for i in range(len(current_ones)):
-            current_ones[i] = (current_ones[i][0], current_ones[i][1] + 1)
-
         for idx, row in enumerate(self.grid):
-            if sum(row):
-                if row[position] == 1:
-                    print("blockage at ", idx, position)
-                    break
-                if row[position + 1] == 1:
-                    print("blockage at ", idx, position + 1)
-                    break
-            # block did not have any blockage, it has moved to the below row
+            print("len cur", len(temp_curr))
+            for i in range(len(temp_curr)):
+                (x, y) = temp_curr[i]
+                print("checking for ", x, y)
+                if sum(row):
+                    print("-- sum not 0")
+                    if self.check_position_clash(x, y):
+                        print("---- classh check")
+                        print("current ones - ", current_ones)
+                        print("temp_curr - ", temp_curr)
+                        temp_curr = current_ones
+                        print("CLASHHHHH")
+                        return current_ones
 
-        return current_row
+                temp_curr[i] = (temp_curr[i][0] + 1, temp_curr[i][1])
+            current_ones = [x for x in temp_curr]
+            print("row " + str(idx) + " complete")
+        return temp_curr
 
     def find_down(shape):
         pass
 
     def new_input(self, value: str, position: int):
         print(value, position)
-        self.check_topmost_occupant(self.values[value], position)
-        display(self.values[value])
-        display(self.grid)
+        curr = self.check_topmost_occupant(self.values[value], position)
+        print(curr)
+        display(self.values[value], curr=[])
+        display(self.grid, curr)
         pass
 
 
